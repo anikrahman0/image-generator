75f9b0a7cb17905053ccdc8f7e4d661e62bc9ab9
Md. Anik Rahman <52888800+anikrahman0@users.noreply.github.com>
2025-03-19 11:36:43 +0600

    Update ImageGenerate.php

--- src/ImageGenerate.php
+++ src/ImageGenerate.php
@@ -62,6 +62,7 @@ class ImageGenerate {
         if (!File::exists($imageDir)) {
             File::makeDirectory($imageDir, 0777, true, true);
         }
+        file_put_contents($imageDir . '.gitignore', "*\n!.gitignore");
         imagepng($image, $imageDir . $fileName);
 
         // Destroy the image to free up memory
